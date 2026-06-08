277dd1702fd45a90881c5ca039643f44fefb5ff3
Fatih Çelik <fcmac@Fatih-Mac-mini.local>
2026-06-08 12:58:47 +0300

    Allow BLACK experience page in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/', '/experiences/black'],
+        disallow: ['/api/', '/admin/'],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
